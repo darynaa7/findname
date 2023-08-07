fe8fb1187672195ee848b5a0b5a05b3e4a4cc6f7
DESKTOP-30CD8TN\Admin <darynaant7@gmail.com>
2023-08-07 19:37:58 +0300

    edit run.bat file to build the library with runlib.bat

--- findingfile/run.bat
+++ findingfile/run.bat
@@ -1,7 +1,10 @@
-@echo off
-rmdir /s /q build
-mkdir build
-cd  build
+cd \AppLib\findfile\Lib
+call runlib.bat
+
+cd \AppLib\findfile\App
+rmdir /s /q build_app
+mkdir build_app
+cd  build_app
 cmake ..
 cmake --build .
 start bin\Debug\app.exe
